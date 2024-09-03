103d9ff6a155ef29f5d5066f6d8fb02872c79177
TGrandeZA <Tuxymucheki@gmail.com>
2024-09-03 02:07:00 +0200

    Need to fix issues of pages opening recursively properly.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,26 @@
-from textnode import TextNode
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-#TextNode testing 
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
-    testObject = TextNode( 
-    text = "This is a test",
-    text_type = "bold", 
-    url = "https://www.youtube.com"
-    )
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
-    print(testObject)
 
 main()
-
-
-
